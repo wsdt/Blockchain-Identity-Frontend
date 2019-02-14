37bfaec0087534e2830d817e0417cba22fcdc4a1
wsdt <kevin.riedl.privat@gmail.com>
2019-02-14 18:52:37 +0100

    linkedin redirect uri etc.

--- s_frontend/lib/controllers/oauth/linkedin/linkedin.dart
+++ s_frontend/lib/controllers/oauth/linkedin/linkedin.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:linkedin_login/linkedin_login.dart';
 
 class OAuth2Linkedin extends StatelessWidget {
-  static const REDIRECT_URL = "#";
+  //static const REDIRECT_URL = "intent://codeing.io/oauth2/linkedin#Intent;scheme=http;package=io.flutter.app.FlutterApplication;category=android.intent.category.BROWSABLE;component=io.flutter.app.FlutterApplication.MainActivity;action=android.intent.action.VIEW;end";
+  static const REDIRECT_URL = "https://codeing.io";
   static const CLIENT_ID = "867keehgjcqcws";
   static const CLIENT_SECRET = "HtHpCG3CBSSEuyfT";
 
