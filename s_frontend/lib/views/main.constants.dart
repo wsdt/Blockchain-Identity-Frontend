69db4da79c5286cb7fb3eaf93f1182124114b3d6
wsdt <kevin.riedl.privat@gmail.com>
2019-02-17 14:30:11 +0100

    email works now

--- s_frontend/lib/views/main.constants.dart
+++ s_frontend/lib/views/main.constants.dart
@@ -1,3 +1,3 @@
 
 const String AUTH_REASON = "Please authenticate to log in into VID-Card.";
-const String BACKEND_URI = "localhost:8080";
+const String BACKEND_URI = "http://10.0.0.22:8080";
--- s_frontend/lib/views/main/verification_methods/verification_email.dart
+++ s_frontend/lib/views/main/verification_methods/verification_email.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -61,36 +63,14 @@ class VerificationEmailState extends State<VerificationEmail> {
                   CupertinoButton(
                       child: Text("Verifiziere E-Mail"),
                       onPressed: () {
-                        FocusScope.of(context).requestFocus(new FocusNode()); // minimize keyboard to loose focus (snackbar height wrong if not minimized)
+                        FocusScope.of(context).requestFocus(
+                            new FocusNode()); // minimize keyboard to loose focus (snackbar height wrong if not minimized)
                         BuildContext ctx =
                             _formKey.currentState.context; // get scaffold state
                         // validate will return true if the form is valid
                         if (_formKey.currentState.validate()) {
                           // TODO: only show success dialog if server response ok (async, await)
-                          this.informServerToSendMail();
-
-                          showDialog(context: context, builder: (_) => CupertinoAlertDialog(
-                            title: Text("E-Mail unterwegs!"),
-                            content: Text(
-                                "Wir haben dir eine E-Mail gesandt. Bitte klicke auf den Link, um dich über deine Mail zu verifizieren."),
-                            actions: <Widget>[
-                              CupertinoButton(
-                                child: const Text("Mail erneut senden"),
-                                onPressed: () {
-                                  Navigator.of(context, rootNavigator: true).pop(
-                                      "Discard"); // benötigt da sonst dialog nicht dismissed
-                                },
-                              ),
-                              CupertinoButton(
-                                child: const Text("Ok"),
-                                onPressed: () {
-                                  Navigator.of(context, rootNavigator: true).pop(
-                                      "Discard"); // benötigt da sonst dialog nicht dismissed
-                                },
-                              ),
-                            ],
-                          ));
-
+                          this.informServerToSendMail(context);
                         } else {
                           Scaffold.of(ctx).showSnackBar(
                             SnackBar(
@@ -104,15 +84,61 @@ class VerificationEmailState extends State<VerificationEmail> {
             )));
   }
 
-  void informServerToSendMail() async {
-    // TODO
+  void informServerToSendMail(BuildContext context) async {
     // If multiple requests use Client instead (packages/http)
-    print("Email: "+emailController.text);
-    http.Response res = await http.post(BACKEND_URI, body: {
-      "email":emailController.text
-    });
+    print("Email: " + emailController.text);
+    http.Response res = await http.post(BACKEND_URI + "/api/v1/verify_mail",
+        encoding: Encoding.getByName("UTF-8"),
+        headers: {"content-type":"application/json"},
+        body: json.encode({"email": emailController.text}));
 
-    print("Response status: ${res.statusCode}");
-    print("Response body: ${res.body}");
+    print(
+        "VerificationEmail:informServerToSendMail: Response status: ${res.statusCode}");
+    print(
+        "VerificationEmail:informServerToSendMail: Response body: ${res.body}");
+
+    if (res.statusCode == 200) {
+      showDialog(
+          context: context,
+          builder: (_) => CupertinoAlertDialog(
+                title: Text("E-Mail unterwegs!"),
+                content: Text(
+                    "Wir haben dir eine E-Mail gesandt. Bitte klicke auf den Link, um dich über deine Mail zu verifizieren."),
+                actions: <Widget>[
+                  CupertinoButton(
+                    child: const Text("Mail erneut senden"),
+                    onPressed: () {
+                      informServerToSendMail(context); // call again
+                      Navigator.of(context, rootNavigator: true).pop(
+                          "Discard"); // benötigt da sonst dialog nicht dismissed
+                    },
+                  ),
+                  CupertinoButton(
+                    child: const Text("Ok"),
+                    onPressed: () {
+                      Navigator.of(context, rootNavigator: true).pop(
+                          "Discard"); // benötigt da sonst dialog nicht dismissed
+                    },
+                  ),
+                ],
+              ));
+    } else {
+      showDialog(
+          context: context,
+          builder: (_) => CupertinoAlertDialog(
+                title: Text("Oops.."),
+                content: Text(
+                    "Aufgrund eines Server-Fehlers konnte deine E-Mail leider nicht gesendet werden. Sollte das Problem weiterhin bestehen, kontaktiere bitte den Administrator."),
+                actions: <Widget>[
+                  CupertinoButton(
+                    child: const Text("Ok"),
+                    onPressed: () {
+                      Navigator.of(context, rootNavigator: true).pop(
+                          "Discard"); // benötigt da sonst dialog nicht dismissed
+                    },
+                  ),
+                ],
+              ));
+    }
   }
 }
